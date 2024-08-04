cc77491b2dcf3263f0d93e9364eb737307a56efa
Tayebe kavousi <tayebekavousi68@gmail.com>
2024-08-04 10:39:54 +0800

    test

--- main.tf
+++ main.tf
@@ -2,480 +2,268 @@ provider "aws" {
   region = var.aws_region
 }
 
-# VPC
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  tags = {
-    Name = "ecs_vpc"
+# Data source for availability zones
+data "aws_availability_zones" "available" {}
+
+# Create the EKS cluster
+resource "aws_eks_cluster" "example" {
+  name     = "example-cluster"
+  role_arn  = aws_iam_role.eks_role.arn
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+  version   = "1.30"  
+
+  vpc_config {
+    subnet_ids = aws_subnet.public.*.id
   }
-}
 
-# INTERNET GATEWAY
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-resource "aws_route_table_association" "public_subnet_1_association" {
-  subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "public_subnet_2_association" {
-  subnet_id      = aws_subnet.public_subnet_2.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-# SUBNETS
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.subnet_1_cidr
-  availability_zone       = var.availability_zone_1
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "public1"
-  }
-}
-
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.subnet_2_cidr
-  availability_zone       = var.availability_zone_2
-  map_public_ip_on_launch = true
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_service_policy,
+    aws_iam_role_policy_attachment.eks_resource_policy
+  ]
 
   tags = {
-    Name = "public2"
+    Name = "example-cluster"
   }
 }
 
-# SECURITY GROUP
-resource "aws_security_group" "ecs_sg" {
-  name        = "ecs_sg"
-  description = "Allow traffic to ECS tasks"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "ecs-sg"
-  }
-}
-
-# CLOUDWATCH LOGS
-resource "aws_cloudwatch_log_group" "ecs_backend" {
-  name = "/ecs/backend"
-}
-
-resource "aws_cloudwatch_log_group" "ecs_frontend" {
-  name = "/ecs/frontend"
-}
-
-resource "aws_cloudwatch_log_group" "ecs_db" {
-  name = "/ecs/db"
-}
-
-# ECS CLUSTER
-resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "My_ecs-cluster"
-}
-
-# ECS IAM ROLE
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
+# Create an IAM role for EKS
+resource "aws_iam_role" "eks_role" {
+  name = "eks-cluster-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "ecs_task_execution_policy" {
-  name       = "ecs-task-execution-policy"
-  roles      = [aws_iam_role.ecs_task_execution_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-# ECS TASK DEFINITIONS
-resource "aws_ecs_task_definition" "my_task" {
-  family                   = "my-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
-  memory                   = "1024"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-
-  container_definitions = jsonencode([
-    {
-      name         = "db"
-      image        = var.db_container_image
-      essential    = true
-      portMappings = [{
-        containerPort = 3306
-      }]
-      environment = [
-        {
-          name  = "MYSQL_ROOT_PASSWORD"
-          value = var.mysql_root_password
-        },
-        {
-          name  = "MYSQL_USER"
-          value = var.mysql_user
-        },
-        {
-          name  = "MYSQL_PASSWORD"
-          value = var.mysql_password
-        },
-        {
-          name  = "MYSQL_DATABASE"
-          value = var.mysql_database
-        }
-      ]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options   = {
-          "awslogs-group"         = aws_cloudwatch_log_group.ecs_db.name
-          "awslogs-region"        = var.aws_region
-          "awslogs-stream-prefix" = "ecs"
-        }
-      }
-    },
-    {
-      name         = "backend"
-      image        = var.backend_container_image
-      essential    = true
-      portMappings = [{
-        containerPort = 5000
-      }]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options   = {
-          "awslogs-group"         = aws_cloudwatch_log_group.ecs_backend.name
-          "awslogs-region"        = var.aws_region
-          "awslogs-stream-prefix" = "ecs"
-        }
-      }
-    },
-    {
-      name         = "frontend"
-      image        = var.frontend_container_image
-      essential    = true
-      portMappings = [{
-        containerPort = 3000
-      }]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options   = {
-          "awslogs-group"         = aws_cloudwatch_log_group.ecs_frontend.name
-          "awslogs-region"        = var.aws_region
-          "awslogs-stream-prefix" = "ecs"
-        }
-      }
-    }
-  ])
-  depends_on = [
-    aws_cloudwatch_log_group.ecs_backend,
-    aws_cloudwatch_log_group.ecs_frontend,
-    aws_cloudwatch_log_group.ecs_db
-  ]
-}
-############################################
-# APPLICATION LOAD BALANCER
-############################################
-resource "aws_lb" "alb" {
-  name               = "ecs-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.ecs_sg.id]
-  subnets            = [
-    aws_subnet.public_subnet_1.id,
-    aws_subnet.public_subnet_2.id
-  ]
-}
-
-resource "aws_lb_target_group" "frontend_target_group_blue" {
-  name     = "frontend-tg-blue"
-  port     = 3000
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-  target_type = "ip"
-  depends_on = [aws_lb.alb]
-}
-
-resource "aws_lb_target_group" "frontend_target_group_green" {
-  name     = "frontend-tg-green"
-  port     = 3000
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-  target_type = "ip"
-  depends_on = [aws_lb.alb]
-}
-
-resource "aws_lb_listener" "frontend_listener_blue" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.frontend_target_group_blue.arn
-  }
-}
-
-resource "aws_lb_listener" "frontend_listener_green" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = "8080"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.frontend_target_group_green.arn
-  }
-}
-############################################
-# ECS SERVICES Blue
-############################################
-resource "aws_ecs_service" "my_app_service_blue" {
-  name            = "my-app-service_blue"
-  cluster         = aws_ecs_cluster.ecs_cluster.id
-  task_definition = aws_ecs_task_definition.my_task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets         = [
-      aws_subnet.public_subnet_1.id,
-      aws_subnet.public_subnet_2.id
-    ]
-    security_groups = [aws_security_group.ecs_sg.id]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.frontend_target_group_blue.arn
-    container_name   = "frontend"
-    container_port   = 3000
-  }
-  deployment_controller {
-    type = "CODE_DEPLOY"
-  }
-}
-
-############################################
-# ECS SERVICES Green
-############################################
-resource "aws_ecs_service" "my_app_service_green" {
-  name            = "my-app-service_green"
-  cluster         = aws_ecs_cluster.ecs_cluster.id
-  task_definition = aws_ecs_task_definition.my_task.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets         = [
-      aws_subnet.public_subnet_1.id,
-      aws_subnet.public_subnet_2.id
-    ]
-    security_groups = [aws_security_group.ecs_sg.id]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.frontend_target_group_green.arn
-    container_name   = "frontend"
-    container_port   = 3000
-  }
-  deployment_controller {
-    type = "CODE_DEPLOY"
-  }
-}
-
-######################################################################
-# AWS CODEDEPLOY
-######################################################################
-
-# CODEDEPLOY IAM ROLE
-resource "aws_iam_role" "codedeploy_role" {
-  name = "CodeDeployRole"
-
-  assume_role_policy = jsonencode( {
-   "Version": "2012-10-17",
-   "Statement": [
-     {
-       "Sid": "",
-       "Effect": "Allow",
-       "Principal": {
-         "Service": [
-           "codedeploy.amazonaws.com"
-         ]
-       },
-       "Action": "sts:AssumeRole"
-     }
-   ]
- })
-}
-
-resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
-  role     = aws_iam_role.codedeploy_role.name
-}
-resource "aws_iam_policy_attachment" "codedeploy_ecs_read_only_policy" {
-  name       = "codedeploy-ecs-read-only-policy"
-  roles      = [aws_iam_role.codedeploy_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
-}
-
-resource "aws_iam_policy" "codedeploy_ecs_policy" {
-  name        = "CodeDeployECSPolicy"
-  description = "Policy for CodeDeploy to describe ECS services"
-  
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "ecs:DescribeServices",
-          "ecs:ListServices"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "codedeploy_ecs_policy_attachment" {
-  name       = "codedeploy-ecs-policy-attachment"
-  roles      = [aws_iam_role.codedeploy_role.name]
-  policy_arn = aws_iam_policy.codedeploy_ecs_policy.arn
-}
-
-# CODEDEPLOY
-resource "aws_codedeploy_app" "example" {
-  compute_platform = "ECS"
-  name             = "example"
-}
-
-# CODEDEPLOY deployment group
-resource "aws_codedeploy_deployment_group" "example" {
-  app_name               = aws_codedeploy_app.example.name
-  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
-  deployment_group_name  = "example"
-  service_role_arn       = aws_iam_role.codedeploy_role.arn
-
-  auto_rollback_configuration {
-    enabled = true
-    events  = ["DEPLOYMENT_FAILURE"]
-  }
-
-  blue_green_deployment_config {
-    deployment_ready_option {
-      action_on_timeout = "CONTINUE_DEPLOYMENT"
-    }
-
-    terminate_blue_instances_on_deployment_success {
-      action                           = "TERMINATE"
-      termination_wait_time_in_minutes = 5
-    }
-  }
-
-  deployment_style {
-    deployment_option = "WITH_TRAFFIC_CONTROL"
-    deployment_type   = "BLUE_GREEN"
-  }
-
-  ecs_service {
-    cluster_name = aws_ecs_cluster.ecs_cluster.name
-    service_name = aws_ecs_service.my_app_service_blue.name
-  }
-
-  load_balancer_info {
-    target_group_pair_info {
-      prod_traffic_route {
-        listener_arns = [aws_lb_listener.frontend_listener_blue.arn]
-      }
-
-      target_group {
-        name = aws_lb_target_group.frontend_target_group_blue.name
-      }
-
-      target_group {
-        name = aws_lb_target_group.frontend_target_group_green.name
-      }
-    }
-  }
-}
-##################################################################
-# IAM Policy for CodeDeploy GitHub Access
-##################################################################
-
-resource "aws_iam_policy" "codedeploy_github_access" {
-  name        = "CodeDeployGitHubAccessPolicy"
-  description = "Policy to allow CodeDeploy to access GitHub repository"
-
-  policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
-        Action = [
-          "codecommit:GitPull"  
-        ]
-        Resource = "*"
-      }
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
     ]
+  })
+
+  tags = {
+    Name = "eks-cluster-role"
+  }
+}
+
+# Attach policies to the IAM role
+resource "aws_iam_role_policy_attachment" "eks_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role     = aws_iam_role.eks_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role     = aws_iam_role.eks_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_resource_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_role.name
+}
+
+# Create a Fargate profile
+resource "aws_eks_fargate_profile" "fargate-profile-system" {
+  depends_on = [aws_eks_cluster.example]
+  cluster_name = aws_eks_cluster.example.name
+  fargate_profile_name = "fargate-profile-system"
+  pod_execution_role_arn = aws_iam_role.fargate_pod_execution_role.arn
+
+  subnet_ids = aws_subnet.public.*.id
+
+  timeouts {
+    # For reasons unknown, Fargate profiles can take upward of 20 minutes to
+    # delete! I've never seen them go past 30m, though, so this seems OK.
+    delete                 = "30m"
+  }
+  selector {
+    namespace = "default"
+  }
+
+  selector {
+    namespace = "magicmusic"
+  }
+
+  selector {
+    namespace = "kube-system"
+    labels = {
+      k8s-app = "kube-dns"
+    }
+  }
+}
+
+# Create an IAM role for Fargate
+resource "aws_iam_role" "fargate_pod_execution_role" {
+  name = "fargate-pod-execution-role"
+  description = "Allow cluster to manage node groups, fargate nodes and cloudwatch logs"
+  force_detach_policies = true
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "eks.amazonaws.com",
+          "eks-fargate-pods.amazonaws.com"
+          ]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+
+  tags = {
+    Name = "fargate-pod-execution-role"
+  }
+}
+
+resource "aws_iam_role" "eks_fargate_role" {
+  name = "fargate-cluster_role"
+  description = "Allow fargate cluster to allocate resources for running pods"
+  force_detach_policies = true
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "eks.amazonaws.com",
+          "eks-fargate-pods.amazonaws.com"
+          ]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+# Attach policies to the IAM role
+resource "aws_iam_role_policy_attachment" "fargate_pod_execution_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role     = aws_iam_role.eks_fargate_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSFargatePodExecutionRolePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.eks_fargate_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_fargate_role.name
+}
+
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_fargate_role.name
+}
+
+
+# Create VPC (or use an existing VPC and subnets)
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "example-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = 2
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.${count.index}.0/24"
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+
+  tags = {
+    Name = "example-public-subnet-${count.index}"
+  }
+}
+
+resource "aws_security_group" "eks" {
+  vpc_id = aws_vpc.example.id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "example-security-group"
+  }
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.example.name
+}
+
+
+
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.example.name
+  node_group_name = "example-node_group"
+  node_role_arn   = aws_iam_role.eks_node_group_role.arn
+  subnet_ids      = aws_subnet.public.*.id
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 2
+  }
+
+  instance_types  = ["t2.small"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
+
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "eks_node_group_role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
   })
 }
 
-resource "aws_iam_role_policy_attachment" "codedeploy_github_access_attachment" {
-  policy_arn = aws_iam_policy.codedeploy_github_access.arn
-  role       = aws_iam_role.codedeploy_role.name 
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_group_role.name
 }
